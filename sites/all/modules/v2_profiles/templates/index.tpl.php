3ed2b625c2194802f01a0c49b3d10fe3a216aa54
Luke <lukewebdev@gmail.com>
2015-01-21 15:25:21 -0600

    debug parent profile

--- sites/all/modules/v2_profiles/templates/index.tpl.php
+++ sites/all/modules/v2_profiles/templates/index.tpl.php
@@ -13,7 +13,7 @@ if($data->user->user_type == 'parent') {
 	<?php print v2_profiles_whiteboard_index($data->user->uid); ?>
 
 <?php } elseif($data->user->user_type == 'parent' || $data->user->user_type == 'teacher') { ?>
-  <?php //print $data->user->user_type; ?>
+  <?php print $data->user->user_type; ?>
 	<?php print v2_profiles_resource_center_index($data->user->uid); ?>
 	<?php print v2_profiles_whiteboard_index($data->user->uid); ?>  
 	<?php print v2_profiles_news_index($data->user->uid); ?>
