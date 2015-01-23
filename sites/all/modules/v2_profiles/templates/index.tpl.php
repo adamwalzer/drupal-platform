d94a1b2e13ca1972d1b3938a9d1636542c081770
Luke <lukewebdev@gmail.com>
2015-01-23 05:45:20 -0600

    debug parent profile

--- sites/all/modules/v2_profiles/templates/index.tpl.php
+++ sites/all/modules/v2_profiles/templates/index.tpl.php
@@ -7,13 +7,13 @@ if($data->user->user_type == 'parent') {
 	}
 }
 ?>
-
+  <?php print_r($data->user);exit; ?>
 <?php if ($data->user->user_type == 'child') { ?>
 	<?php print v2_profiles_flips_index($data->user->uid); ?>
 	<?php print v2_profiles_whiteboard_index($data->user->uid); ?>
 
 <?php } elseif($data->user->user_type == 'parent' || $data->user->user_type == 'verified-parent' || $data->user->user_type == 'teacher') { ?>
-  <?php print_r($data->user);exit; ?>
+
 	<?php print v2_profiles_resource_center_index($data->user->uid); ?>
 	<?php print v2_profiles_whiteboard_index($data->user->uid); ?>  
 	<?php print v2_profiles_news_index($data->user->uid); ?>
