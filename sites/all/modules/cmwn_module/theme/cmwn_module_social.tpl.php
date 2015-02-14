2c7a8c3dfdd01f08563394fcde37d4fcb1689acf
Luke <lukewebdev@gmail.com>
2015-02-13 19:16:26 -0600

    remvoed contributed modules

--- sites/all/modules/cmwn_module/theme/cmwn_module_social.tpl.php
+++
@@ -1,6 +0,0 @@
-<div class="social">
-  <a href="http://www.facebook.com/ChangeMyWorldNow" class="facebook" title="Friend us on Facebook">Facebook</a>
-  <a href="http://twitter.com/CMyWorldNow" class="twitter" title="Follow us on Twitter">Twitter</a>
-  <!--a href="" class="linkedin" title="Friend us on LinkedIn">LinkedIn</a-->
-  <a href="http://www.flickr.com/photos/cassandras_angel/" class="flickr" title="Find us on Flickr">Flickr</a>
-</div>
--- sites/all/modules/v2_profiles/templates/children/read.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* 1.4 */ ?>
-
-<?php print theme('v2_profiles_children_index', $data); ?>
