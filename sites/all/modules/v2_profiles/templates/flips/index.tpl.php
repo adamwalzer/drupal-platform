de69551b2918600e772f25736bcdfc5b6aea546c
Luke Meier <lukewebdev@gmail.com>
2015-07-08 22:24:57 +0000

    flip loading graphic

--- sites/all/modules/v2_profiles/templates/flips/index.tpl.php
+++ sites/all/modules/v2_profiles/templates/flips/index.tpl.php
@@ -1,6 +1,15 @@
 <?php /* 1.4 */ ?>
 
-<iframe class="block shadow" height="221px" width="100%" src="http://aiselector.changemyworldnow.com?uid=<?php print $data->user->uid;?>" border="0" scrolling="no" scrollbars="none" frameborder="0"></iframe>
+<style>
+#loadImg{position:absolute;z-index:999;text-align: center;left: 480px;}
+#loadImg div{display:table-cell;width:100%;height:20px;top: 20px;text-align:center;vertical-align:middle;box-sizing: border-box;padding: 40px;}
+</style>
+<div id="loadImg"><div><img src="/sites/all/themes/cmwn/assets/img/CMW2.gif" style="width: auto;height: 200px;opacity: .5;" /></div></div>
+<iframe name="iframe" class="block shadow" height="221px" width="100%" src="http://aiselector.changemyworldnow.com?uid=<?php print $data->user->uid;?>" border="0" scrolling="no" scrollbars="none" frameborder="0" onload="document.getElementById('loadImg').style.display='none';"></iframe>
+
+
+
+
 <?php if(false):?>
 <div class="block shadow flips browse index">
 
