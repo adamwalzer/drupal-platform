1f8652d5937454837247ccc5431de902afd5b277
Luke <lukewebdev@gmail.com>
2015-10-14 11:30:42 -0500

    eaningless change to test pull into test removing game field

--- sites/all/modules/v2_profiles/templates/flips/read.tpl.php
+++ sites/all/modules/v2_profiles/templates/flips/read.tpl.php
@@ -3,7 +3,7 @@ if($data->user->user_type != 'child') {
 	header('Location: /user/');
 	exit();
 }
-?>
+?> 
 <div class="block flips action <?php print $data->node->color; ?>">
 	
 		<iframe class="gameframe" src="/<?php print $data->node->field_game_filepath[0]['value'];?>" scrolling="no" frameborder="0" width="624" height="624"></iframe>	
