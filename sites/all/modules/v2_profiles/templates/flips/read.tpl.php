6546c6c4e601812518b4eb3c6cec9d52941f4072
Luke <lukewebdev@gmail.com>
2015-10-14 10:47:14 -0500

    remove shadow from game page

--- sites/all/modules/v2_profiles/templates/flips/read.tpl.php
+++ sites/all/modules/v2_profiles/templates/flips/read.tpl.php
@@ -4,7 +4,7 @@ if($data->user->user_type != 'child') {
 	exit();
 }
 ?>
-<div class="block shadow flips action <?php print $data->node->color; ?>">
+<div class="block flips action <?php print $data->node->color; ?>">
 	
 		<iframe class="gameframe" src="/<?php print $data->node->field_game_filepath[0]['value'];?>" scrolling="no" frameborder="0" width="624" height="624"></iframe>	
 	
