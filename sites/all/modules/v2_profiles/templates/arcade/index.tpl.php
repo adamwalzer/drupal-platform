10f876348036864c8f9bb95fa8fc0d499f5c1994
Luke <lukewebdev@gmail.com>
2015-10-16 09:17:56 -0500

    chrome browser msg for arcade

--- sites/all/modules/v2_profiles/templates/arcade/index.tpl.php
+++ sites/all/modules/v2_profiles/templates/arcade/index.tpl.php
@@ -11,7 +11,16 @@
   <div id="arcade-tab">
   	<?php if($user->user_type == 'child' || $user->user_type == 'parent' || $user->user_type == 'teacher') { ?>
   	<div class="items">
-    <iframe width=624 style="height:1500px;border: 0;" scrolling="no" border=0 src="http://arcade.changemyworldnow.com?uid=<?php print $user->uid; ?>"></iframe>
+    <?php if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== false || strpos($_SERVER['HTTP_USER_AGENT'], 'CriOS') !== false):?>
+      <div class="arcade-error">
+        Hold on!
+        This game is best experienced
+        using Internet Explorer or Safari.
+      </div>
+    <?php else: ?>
+      <iframe width=624 style="height:1500px;border: 0;" scrolling="no" border=0 src="http://arcade.changemyworldnow.com?uid=<?php print $user->uid; ?>"></iframe>
+    <?php endif; ?>
+    
   		  
   	</div>
   	<?php  }?>
