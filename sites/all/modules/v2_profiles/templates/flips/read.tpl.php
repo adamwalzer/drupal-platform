c053fcafd4566de141c76274d33ebd506ac3be78
Luke <lukewebdev@gmail.com>
2015-10-19 03:12:07 -0500

    notifications - remove child notifications from parent main account window #161

--- sites/all/modules/v2_profiles/templates/flips/read.tpl.php
+++ sites/all/modules/v2_profiles/templates/flips/read.tpl.php
@@ -5,7 +5,5 @@ if($data->user->user_type != 'child') {
 }
 ?> 
 <div class="block flips action <?php print $data->node->color; ?>">
-	
 		<iframe class="gameframe" src="/<?php print $data->node->field_game_filepath[0]['value'];?>" scrolling="no" frameborder="0" width="624" height="624"></iframe>	
-	
 </div>
